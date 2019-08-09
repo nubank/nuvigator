f3dd4f134df7b63f2569bfff04ffc99d34009170
William Cho <williamhjcho@gmail.com>
2019-08-09 18:38:14 -0300

    Migrates common test files to common_test package

--- lib/src/screen.dart
+++ lib/src/screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:nubank/nuds/nuds.dart';
+import 'package:nuds/nuds.dart';
 import 'package:provider/provider.dart';
 
 import 'navigation_service.dart';
