81ed5538cb910467a86e06bda071185a84a24429
lucas.britto <l_demello@hotmail.com>
2019-10-10 18:15:32 -0300

    Refactor code to use code builder

--- lib/builder.dart
+++ lib/builder.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
 
-import 'package:build/build.dart';
 import 'package:analyzer/dart/element/element.dart';
-import 'package:source_gen/source_gen.dart';
 import 'package:analyzer/dart/element/type.dart';
+import 'package:build/build.dart';
+import 'package:code_builder/code_builder.dart';
+import 'package:dart_style/dart_style.dart';
+import 'package:source_gen/source_gen.dart';
+
+final _dartfmt = DartFormatter();
 
 class NuRouter {
   const NuRouter();
@@ -36,103 +40,269 @@ String capitalize(String s) => s[0].toUpperCase() + s.substring(1);
 String lowerCamelCase(String s) => s[0].toLowerCase() + s.substring(1);
 
 class NuvigationGenerator extends GeneratorForAnnotation<NuRouter> {
+  Method screensMapMethod(ClassElement classElement, String code) {
+    final className = classElement.name;
+    final lowerClassName = lowerCamelCase(classElement.name);
+
+    return Method(
+      (b) => b
+        ..body = const Code('')
+        ..name = '$lowerClassName\$getScreensMap'
+        ..requiredParameters.add(
+          Parameter(
+            (p) => p
+              ..type = refer(className)
+              ..name = 'router',
+          ),
+        )
+        ..returns = refer('Map<String, Screen>')
+        ..body = Code('return {$code};'),
+    );
+  }
+
+  Method subRoutersListMethod(ClassElement classElement) {
+    final className = classElement.name;
+    final lowerClassName = lowerCamelCase(classElement.name);
+
+    return Method(
+      (b) => b
+        ..body = const Code('')
+        ..name = '$lowerClassName\$getSubRoutersList'
+        ..requiredParameters.add(
+          Parameter(
+            (p) => p
+              ..type = refer(className)
+              ..name = 'router',
+          ),
+        )
+        ..returns = refer('List<Router>'),
+    );
+  }
+
+  Class generateNavigationClass(
+      ClassElement classElement, List<Method> methods) {
+    final className = classElement.name;
+    final lowerClassName = lowerCamelCase(classElement.name);
+
+    return Class(
+      (b) => b
+        ..name = '${className}Navigation'
+        ..constructors.add(
+          Constructor(
+            (c) => c
+              ..requiredParameters.add(
+                Parameter(
+                  (p) => p
+                    ..name = 'nuvigator'
+                    ..toThis = true,
+                ),
+              ),
+          ),
+        )
+        ..fields.add(
+          Field(
+            (f) => f
+              ..name = 'nuvigator'
+              ..type = refer('NuvigatorState')
+              ..modifier = FieldModifier.final$,
+          ),
+        )
+        ..methods.addAll(
+          [
+            Method(
+              (m) => m
+                ..name = 'of'
+                ..requiredParameters.add(
+                  Parameter(
+                    (p) => p
+                      ..name = 'context'
+                      ..type = refer('BuildContext'),
+                  ),
+                )
+                ..returns = refer(b.name)
+                ..body = Code('${b.name}(Nuvigator.of(context))')
+                ..lambda = true
+                ..static = true,
+            ),
+            ...methods,
+          ],
+        ),
+    );
+  }
+
+  Class generateRoutesClass(ClassElement classElement, List<Field> fields) {
+    final className = classElement.name;
+    final lowerClassName = lowerCamelCase(classElement.name);
+
+    return Class(
+      (b) => b
+        ..name = '${className}Routes'
+        ..fields.addAll(fields),
+    );
+  }
+
   @override
   FutureOr<String> generateForAnnotatedElement(
       Element element, ConstantReader annotation, BuildStep buildStep) {
-    final buffer = StringBuffer();
-    final screensMapBuffer = StringBuffer();
-    final subRoutersListBuffer = StringBuffer();
-    final navigationBuffer = StringBuffer();
-    final argsClassesBuffer = StringBuffer();
     final ClassElement classElement = element;
-//    buffer.write("import 'package:nuvigator/nuvigator.dart';\n");
-//    buffer.write("import 'package:flutter/widgets.dart';\n");
-//    buffer.write("import '${classElement.source.shortName}';\n\n");
-    buffer.write('class ${classElement.name}Routes {\n');
-    screensMapBuffer.write(
-        'Map<String, Screen> ${lowerCamelCase(classElement.name)}\$getScreensMap(${classElement.name} router) {\n'
-        'return {\n');
-    subRoutersListBuffer.write(
-        'List<Router> ${lowerCamelCase(classElement.name)}\$getSubRoutersList(${classElement.name} router) {\n'
-        'return [\n');
-    navigationBuffer.write('class ${classElement.name}Navigation {\n'
-        '${classElement.name}Navigation(this.nuvigator);\n\n'
-        'static ${classElement.name}Navigation of(BuildContext context) => ${classElement.name}Navigation(Nuvigator.of(context));\n\n'
-        'final NuvigatorState nuvigator;\n\n');
+    final className = classElement.name;
+    final lowerClassName = lowerCamelCase(classElement.name);
+
+    final screensMapBuffer = StringBuffer();
+
+    final listSpecs = <Spec>[];
+    final fieldsRoutesClass = <Field>[];
+    final methodsNavigationClass = <Method>[];
+
     for (final field in classElement.fields) {
       String navigationFnParams = '';
       final nuRouteFieldAnnotation =
           _nuRouteChecker.firstAnnotationOfExact(field);
       final nuSubRouterAnnotation =
           _nuSobRouterChecker.firstAnnotationOfExact(field);
+
       if (nuRouteFieldAnnotation != null) {
         final argsBuffer = StringBuffer();
         final args = nuRouteFieldAnnotation?.getField('args')?.toMapValue();
+
         final subRouter =
             nuRouteFieldAnnotation?.getField('subRouter')?.toTypeValue();
-        if (args != null) {
-          final argsFieldsBuffer = StringBuffer();
-          final argsParserBuffer = StringBuffer();
-          final argsClassName = '${capitalize(field.name)}Args';
-          argsClassesBuffer.write('class $argsClassName {\n'
-              '${capitalize(field.name)}Args(');
 
-          argsBuffer.write('{');
+        if (args != null) {
+          final argsParserBuffer = StringBuffer('');
+          final argsClassName = '${capitalize(field.name)}Args';
+
+          final parametersConstructor = <Parameter>[];
+          final fieldsArgs = <Field>[];
+
           for (final argEntry in args.entries) {
             final typeName = argEntry.value.toTypeValue().name;
             final varName = argEntry.key.toStringValue();
-            argsBuffer.write('@required this.$varName,');
+
             argsParserBuffer.write("$varName: args['$varName'],\n");
-            argsFieldsBuffer.write('final $typeName $varName;\n');
+
+            parametersConstructor.add(
+              Parameter(
+                (p) => p
+                  ..name = varName
+                  ..named = true
+                  ..annotations.add(
+                    const CodeExpression(
+                      Code('required'),
+                    ),
+                  )
+                  ..toThis = true,
+              ),
+            );
+
+            fieldsArgs.add(
+              Field(
+                (m) => m
+                  ..name = varName
+                  ..type = refer(typeName)
+                  ..modifier = FieldModifier.final$,
+              ),
+            );
           }
-          argsBuffer.write('}');
 
-          argsClassesBuffer.write(argsBuffer.toString());
-          argsClassesBuffer.write(');\n\n');
-          argsClassesBuffer.write(argsFieldsBuffer.toString());
+          final argsClass = Class(
+            (c) => c
+              ..name = argsClassName
+              ..constructors.add(
+                Constructor(
+                  (c) => c..optionalParameters.addAll(parametersConstructor),
+                ),
+              )
+              ..fields.addAll(fieldsArgs)
+              ..methods.add(
+                Method(
+                  (m) => m
+                    ..name = 'parse'
+                    ..requiredParameters.add(
+                      Parameter((p) => p
+                        ..name = 'args'
+                        ..type = refer('Map<String, String>')),
+                    )
+                    ..returns = refer(argsClassName)
+                    ..static = true
+                    ..body = Code(
+                        'return $argsClassName(${argsParserBuffer.toString()});'),
+                ),
+              ),
+          );
 
-          argsClassesBuffer.write(
-              '\nstatic $argsClassName parse(Map<String, String> args) {\n'
-              'return $argsClassName(${argsParserBuffer.toString()});');
+          listSpecs.add(argsClass);
 
-          argsClassesBuffer.write('\n}\n');
-
-          argsClassesBuffer.write('\n}\n\n');
-          navigationFnParams = '$argsClassName arguments';
+          navigationFnParams = argsClassName;
         }
+
         final screenReturn = getGenericTypes(field.type);
-        buffer.write(
-            'static const ${field.name} = \'${classElement.name}/${field.name}\';\n');
-        screensMapBuffer.write(
-            '${classElement.name}Routes.${field.name}: router.${field.name},\n');
-        navigationBuffer.write(
-            'Future<$screenReturn> ${field.name}($navigationFnParams) {\n'
-            'return nuvigator.pushNamed<$screenReturn>(${classElement.name}Routes.${field.name});'
-            '\n}\n');
+
+        screensMapBuffer
+            .write('${className}Routes.${field.name}: router.${field.name},\n');
+
+        fieldsRoutesClass.add(
+          Field(
+            (f) => f
+              ..name = field.name
+              ..assignment = Code("'$className/${field.name}'")
+              ..modifier = FieldModifier.constant
+              ..static = true,
+          ),
+        );
+
+        final methodParams = navigationFnParams.isEmpty
+            ? <Parameter>[]
+            : [
+                Parameter(
+                  (p) => p
+                    ..name = 'arguments'
+                    ..type = refer(navigationFnParams),
+                )
+              ];
+
+        methodsNavigationClass.add(
+          Method(
+            (m) => m
+              ..name = field.name
+              ..returns = refer('Future<$screenReturn>')
+              ..requiredParameters.addAll(methodParams)
+              ..body = Code(
+                'return nuvigator.pushNamed<$screenReturn>(${className}Routes.${field.name});',
+              ),
+          ),
+        );
 
         if (subRouter != null) {
-          navigationBuffer.write(
-              '${subRouter.name}Navigation get ${lowerCamelCase(subRouter.name)}Navigation => ${subRouter.name}Navigation(nuvigator);\n');
+          methodsNavigationClass.add(
+            Method(
+              (m) => m
+                ..name = '${lowerCamelCase(subRouter.name)}Navigation'
+                ..returns = refer('${subRouter.name}Navigation')
+                ..type = MethodType.getter
+                ..lambda = true
+                ..body = Code(
+                  '${subRouter.name}Navigation(nuvigator)',
+                ),
+            ),
+          );
         }
       } else if (nuSubRouterAnnotation != null) {
-        subRoutersListBuffer.write('router.${field.name},\n');
-        navigationBuffer.write(
-            '${field.type.name}Navigation get ${lowerCamelCase(field.name)}Navigation => ${field.type.name}Navigation(nuvigator);\n');
+//        subRoutersListBuffer.write('router.${field.name},\n');
+//        navigationBuffer.write(
+//            '${field.type.name}Navigation get ${lowerCamelCase(field.name)}Navigation => ${field.type.name}Navigation(nuvigator);\n');
       }
     }
-    buffer.write('\n}');
-    navigationBuffer.write('\n}');
-    screensMapBuffer.write('\n};\n}');
-    subRoutersListBuffer.write('\n];\n}');
-    return buffer.toString() +
-        '\n' +
-        argsClassesBuffer.toString() +
-        '\n' +
-        screensMapBuffer.toString() +
-        '\n' +
-        subRoutersListBuffer.toString() +
-        '\n' +
-        navigationBuffer.toString();
+
+    final lib = Library((l) => l.body.addAll([
+          generateRoutesClass(classElement, fieldsRoutesClass),
+          generateNavigationClass(classElement, methodsNavigationClass),
+          screensMapMethod(classElement, screensMapBuffer.toString()),
+          ...listSpecs
+        ]));
+    final libCode = _dartfmt.format('${lib.accept(DartEmitter())}');
+    print(libCode);
+    return libCode;
   }
 }
 
