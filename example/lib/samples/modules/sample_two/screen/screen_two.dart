5c970f8fe1b9551819bc2459f79273d9e1621d36
Leonardo Iacovini <leoiacovini@gmail.com>
2019-10-16 15:17:59 -0300

    Remove unused import

--- example/lib/samples/modules/sample_two/screen/screen_two.dart
+++ example/lib/samples/modules/sample_two/screen/screen_two.dart
@@ -1,4 +1,3 @@
-import 'package:example/samples/modules/sample_one/navigation/sample_one_router.dart';
 import 'package:example/samples/navigation/samples_router.dart';
 import 'package:flutter/material.dart';
 import 'package:nuvigator/nuvigator.dart';
