25d217c6da7acbe79f7fcab5de0993a0d86d38a6
lucas.britto <l_demello@hotmail.com>
2019-10-25 11:58:52 -0300

    Add docs to NuRouter and PushMethodType

--- lib/src/annotations.dart
+++ lib/src/annotations.dart
@@ -1,3 +1,38 @@
+/// Annotation used to define a router
+///
+/// This annotation is used by the code generator to identify the class as a
+/// router and start the analysis of code generation.
+///
+/// By default the [NuRouter] generates all files with the class name where the
+/// annotation is used, for example:
+///
+/// @NuRouter()
+/// class MyRouter {}
+///
+/// The generated files will be:
+///
+/// class MyRouterRoutes {
+///   static const myNuRoute = 'myRouter/myNuRoute';
+/// }
+///
+/// MyRouterNavigation get myRouterNavigation => MyRouterNavigation.of(context);
+///
+/// You can specify the generated names using the argument [routerName] and the
+/// [routeNamePrefix]. Here an example:
+///
+/// @NuRouter(routerName: 'Custom', routeNamePrefix: 'customPrefix/')
+/// class MyRouter {}
+///
+/// The generated files will be:
+///
+/// class CustomRoutes {
+///   static const myNuRoute = 'customPrefix/custom/myNuRoute';
+/// }
+///
+/// CustomNavigation get customNavigation => CustomNavigation.of(context);
+///
+/// Obs: The [routeNamePrefix] is only used by the routes class to compose the
+/// route names.
 class NuRouter {
   const NuRouter({this.routerName, this.routeNamePrefix});
 
@@ -13,4 +48,7 @@ class NuRoute {
   final List<PushMethodType> pushMethods;
 }
 
+/// Enum to represent the push methods available on nuvigator
+///
+/// Check the [NuRoute] class to understand how this enum can be useful.
 enum PushMethodType { push, pushReplacement, popAndPush, pushAndRemoveUntil }
