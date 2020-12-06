e2dd6d187b6986dfa837b2ff56db48b3a04feac0
Leonardo Iacovini <leoiacovini@gmail.com>
2020-12-06 16:58:51 -0300

    Format files

--- lib/src/typings.dart
+++ lib/src/typings.dart
@@ -15,8 +15,9 @@ typedef ObserverBuilder = NavigatorObserver Function();
 
 typedef WrapperFn = Widget Function(BuildContext context, Widget child);
 
-typedef NuWidgetRouteBuilder<A extends Object, R extends Object> = Widget Function(
-    BuildContext context, NuRouteBuilder<A, R> nuRoute, NuRouteSettings<A> settings);
+typedef NuWidgetRouteBuilder<A extends Object, R extends Object>
+    = Widget Function(BuildContext context, NuRouteBuilder<A, R> nuRoute,
+        NuRouteSettings<A> settings);
 
 typedef NuRouteParametersParser<A> = A Function(Map<String, dynamic>);
 
