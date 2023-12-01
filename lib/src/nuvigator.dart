7b0ce3333cc8c1da0aea249c4d70b578ceffdab4
NatashaZettler <natasha.zettler@nubank.com.br>
2023-12-01 01:04:37 -0300

    Update nuvigator.dart

--- lib/src/nuvigator.dart
+++ lib/src/nuvigator.dart
@@ -172,6 +172,10 @@ class NuvigatorState<T extends INuRouter> extends NavigatorState
     return widget.inheritableObservers;
   }
 
+  void addObserver(NavigatorObserver observer) {
+    widget.observers.add(observer);
+  }
+  
   @override
   void initState() {
     parent = Nuvigator.of(context, nullOk: true);
